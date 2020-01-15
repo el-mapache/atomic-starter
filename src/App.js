a8a9cf09ee402b4cb9ad36ff1b40807b3245824a
Adam Biagianti <adam.biagianti@gmail.com>
2020-01-14 19:57:09 -0800

    Initial commit of react styled component bootstrap app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './styles/app.css';
+import styled from 'styled-components/macro';
+import tw from 'tailwind.macro';
+
+const Badge = styled.p`
+  ${tw`flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3`}
+`
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Badge>Looking good friend!</Badge>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
